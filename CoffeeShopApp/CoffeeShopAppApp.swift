fd83211cdd4af6e703559e21f7f6b5bd98d51f80
Luciano Gomes <lucianogvda02@gmail.com>
2024-08-07 22:06:52 -0300

    Initial Commit

---
+++ CoffeeShopApp/CoffeeShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopAppApp.swift
+//  CoffeeShopApp
+//
+//  Created by Luciano Gomes on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
