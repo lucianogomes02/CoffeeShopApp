6704e76d94286e351fac66c3814fcb0bf6984d68
Luciano Gomes <lucianogvda02@gmail.com>
2024-08-08 23:40:48 -0300

    refactor: alter all categories icons to grid layout

--- CoffeeShopApp/Views/HomeView.swift
+++ CoffeeShopApp/Views/HomeView.swift
@@ -7,16 +7,85 @@
 
 import SwiftUI
 
+enum Categories: String, CaseIterable {
+    case coffee
+    case desserts
+    case alcohol
+    case food
+}
+
+enum CategoriesLabels: String, CaseIterable {
+    case coffeeLabel = "Café"
+    case dessertsLabel = "Sobremesas"
+    case alcoholLabel = "Alcoólicas"
+    case foodLabel = "Lanches"
+}
+
 enum CategoriesIcons: String, CaseIterable {
-    case coffee = "mug.fill"
-    case desserts = "birthday.cake.fill"
-    case alcohol = "wineglass.fill"
-    case breakfast = "fork.knife.circle.fill"
+    case coffeeIcon = "mug.fill"
+    case dessertsIcon = "birthday.cake.fill"
+    case alcoholIcon = "wineglass.fill"
+    case foodIcon = "fork.knife.circle.fill"
 }
 
 struct HomeView: View {
     @State var coffeeShopSearch: String = ""
-    @State private var selectedCategory: CategoriesIcons?
+    @State private var selectedCategory: Categories?
+    
+    private func selectCategoryLabel(category: String) -> String {
+        switch(category) {
+            case "coffee":
+                return CategoriesLabels.coffeeLabel.rawValue
+            case "desserts":
+                return CategoriesLabels.dessertsLabel.rawValue
+            case "alcohol":
+                return CategoriesLabels.alcoholLabel.rawValue
+            case "food":
+                return CategoriesLabels.foodLabel.rawValue
+            default:
+                return CategoriesLabels.coffeeLabel.rawValue
+            }
+    }
+    
+    private func selectCategoryIcon(category: String) -> String {
+        switch(category) {
+            case "coffee":
+                return CategoriesIcons.coffeeIcon.rawValue
+            case "desserts":
+                return CategoriesIcons.dessertsIcon.rawValue
+            case "alcohol":
+                return CategoriesIcons.alcoholIcon.rawValue
+            case "food":
+                return CategoriesIcons.foodIcon.rawValue
+            default:
+                return CategoriesIcons.coffeeIcon.rawValue
+            }
+    }
+    
+    private func categoryButton(category: Categories) -> some View {
+            Button(action: {
+                selectedCategory = category
+            }) {
+                VStack {
+                    Image(systemName: selectCategoryIcon(category: category.rawValue))
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(selectedCategory == category ? .white : .brown)
+                    Text(selectCategoryLabel(category: category.rawValue))
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundStyle(selectedCategory == category ? .white : .brown)
+                }
+                .frame(width: 150, height: 100)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(selectedCategory == category ? Color.brown : Color.white)
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.brown, lineWidth: 2)
+                )
+            }.padding(8)
+        }
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
@@ -49,32 +118,14 @@ struct HomeView: View {
                     .padding(.leading)
             }
             
-            ScrollView(.vertical, showsIndicators: false) {
-                HStack(spacing: 20) {
-                    ForEach(CategoriesIcons.allCases, id: \.rawValue) { category in
-                        Button(action: {
-                            selectedCategory = category
-                        }) {
-                            VStack {
-                                Image(systemName: category.rawValue)
-                                    .resizable()
-                                    .frame(width: 50, height: 50)
-                                    .foregroundColor(selectedCategory == category ? .white : .brown)
-                            }
-                            .frame(width: 70, height: 70)
-                            .background(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .fill(selectedCategory == category ? Color.brown : Color.white)
-                            )
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.brown, lineWidth: 2)
-                            )
-                            .padding(1)
-                        }
+            LazyHGrid(rows: [
+                GridItem(.fixed(100)),
+                GridItem(.fixed(100))
+            ]) {
+                ForEach(Categories.allCases, id: \.rawValue) { category in
+                        categoryButton(category: category)
                     }
                 }.padding()
-            }
             
             Spacer()
         }
